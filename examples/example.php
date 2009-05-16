bb0bc925a0b59fa246dbe9e61df5eee4af71f601
Arin Sarkissian <arin@skook.local>
2009-05-15 22:38:06 -0700

    clean up exceptions for missing dbs & comment example file

---
+++ examples/example.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Currently all examples are in 1 file cuz this thing's nowhere near ready and 
+ * updating multiple files would be fucking annoying.
+ *
+ * Eventually this will be split into multiple files... each hilighting a different feature
+ */
+require_once('./inc.php');
+require_once('Duckk/CouchDB.php');
+
+$couchdb = new Duckk_CouchDB();
+
+echo "------------LIST OF DATABASES---------------\n";
+print_r($couchdb->getDatabases());
+
+
+$randomDBName = 'testing' . md5(microtime(true));
+echo "------------CREATE A DB NAMED $randomDBName --------------\n";
+var_dump($couchdb->createDatabase($randomDBName));
+echo "------------TRY TO CREATE $randomDBName again --------------\n";
+try {
+    $couchdb->createDatabase($randomDBName);    
+} catch (Exception $e) {
+    print_r($e);
+}
+
+echo "------------DELETE A DB NAMED $randomDBName --------------\n";
+var_dump($couchdb->deleteDatabase($randomDBName));
+echo "------------TRY TO DELETE $randomDBName again --------------\n";
+try {
+    $couchdb->deleteDatabase($randomDBName);    
+} catch (Exception $e) {
+    print_r($e);
+}
+
+?>
